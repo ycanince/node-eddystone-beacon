97b0c78b5a634e0c21bb9eb282157b8f5474d19a
Yusuf Can INCE <can.ince@armongate.com>
2020-12-02 14:12:50 +0300

    @abandonware/bleno

--- examples/connectable/connectable.js
+++ examples/connectable/connectable.js
@@ -2,31 +2,27 @@
 // See https://github.com/don/node-eddystone-beacon/issues/30
 // See https://github.com/don/node-eddystone-beacon/pull/31
 
-var bleno = require('eddystone-beacon/node_modules/bleno'); // require('bleno');
-var eddystoneBeacon = require('eddystone-beacon'); // require('./../../index')
+var bleno = require("eddystone-beacon/node_modules/@abandonware/bleno"); // require('bleno');
+var eddystoneBeacon = require("eddystone-beacon"); // require('./../../index')
 
 var characteristic = new bleno.Characteristic({
-  uuid: 'E0D38F1C-56CA-4B75-9D44-3E4134F7CB0B',
-  properties: ['read'],
-  value: new Buffer('example')
+  uuid: "E0D38F1C-56CA-4B75-9D44-3E4134F7CB0B",
+  properties: ["read"],
+  value: new Buffer("example"),
 });
 
 var service = new bleno.PrimaryService({
-  uuid: 'E0D38F1C-56CA-4B75-9D44-3E4134F7CB0A',
-  characteristics: [
-    characteristic
-  ]
+  uuid: "E0D38F1C-56CA-4B75-9D44-3E4134F7CB0A",
+  characteristics: [characteristic],
 });
 
-bleno.once('advertisingStart', function(err) {
+bleno.once("advertisingStart", function (err) {
   if (err) {
     throw err;
   }
 
-  console.log('on -> advertisingStart');
-  bleno.setServices([
-    service
-  ]);
+  console.log("on -> advertisingStart");
+  bleno.setServices([service]);
 });
 
-eddystoneBeacon.advertiseUrl('https://www.example.com');
+eddystoneBeacon.advertiseUrl("https://www.example.com");
